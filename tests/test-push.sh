6064f5bead5eb9afea6fae735c76f57a111ff568
colin <colinrozzi@gmail.com>
2025-08-03 15:00:09 -0400

    Add test-push.sh script

--- tests/test-push.sh
+++ tests/test-push.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+# This script initializes a Git repository, creates a file, commits it,
+
 mkdir tmp-dir
+cd tmp-dir
+
+git init
+echo "test" > test.txt
+git add test.txt
+git commit -m "Initial commit"
+
+git remote add wasm http://localhost:8080
+git push wasm master
+
+cd ..
+rm -rf tmp-dir
