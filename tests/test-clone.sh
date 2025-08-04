3867f312da2eb9fe9c194ffb4d268a6f6c2dce26
colin <colinrozzi@gmail.com>
2025-08-03 23:25:42 -0400

    test: add clone test script

---
+++ tests/test-clone.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script initializes a Git repository, creates a file, commits it,
+
+mkdir tmp-dir
+cd tmp-dir
+
+git init
+echo "test" > test.txt
+git add test.txt
+git commit -m "Initial commit"
+
+git remote add wasm http://localhost:8080
+GIT_TRACE_PACKET=1 GIT_TRACE=1 GIT_RECEIVE_PACK_DEBUG=1 git push -u wasm main
+
+cd ..
+
+GIT_TRACE_PACKET=1 GIT_TRACE=1 GIT_RECEIVE_PACK_DEBUG=1 git clone http://localhost:8080 tmp-clone
+
